c26583fa0087b48e81809774f6450e7acc000cb6
Cheb <chebTS@gmail.com>
2013-10-26 12:55:09 +0300

    sliding menu works

--- NFCq/src/com/chernicka/MainActivityChernicka.java
+++ NFCq/src/com/chernicka/MainActivityChernicka.java
@@ -1,15 +1,26 @@
 package com.chernicka;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class MainActivityChernicka extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity_chernicka);
+        //setContentView(R.layout.activity_main_activity_chernicka);
+        
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.menu);
+        
     }
 
 
