00218908e2f17ca616218795acbf948a04f86b4f
smokienko <korwin22@gmail.com>
2013-10-26 12:55:49 +0300

    Added models.

--- NFCq/src/com/chernicka/model/Task.java
+++ NFCq/src/com/chernicka/model/Task.java
@@ -1,7 +1,85 @@
 package com.chernicka.model;
 
+import org.json.JSONObject;
+
 /**
  * Created by Sov on 26.10.13.
  */
 public class Task {
+
+    private String partId;
+    private String fullId;
+    private long taskDuration;
+    private int latitude;
+    private int longtitude;
+    private String response;
+    private String taskText;
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+
+        return response;
+    }
+
+    public void setPartId(String partId) {
+        this.partId = partId;
+    }
+
+    public void setFullId(String fullId) {
+        this.fullId = fullId;
+    }
+
+    public void setTaskDuration(long taskDuration) {
+        this.taskDuration = taskDuration;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongtitude(int longtitude) {
+        this.longtitude = longtitude;
+    }
+
+    public void setTaskText(String taskText) {
+        this.taskText = taskText;
+    }
+
+    public String getPartId() {
+        return partId;
+    }
+
+    public String getFullId() {
+        return fullId;
+    }
+
+    public long getTaskDuration() {
+        return taskDuration;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public int getLongtitude() {
+        return longtitude;
+    }
+
+    public String getTaskText() {
+        return taskText;
+    }
+
+    public void setStartTaskDataFromJson(JSONObject object){
+        //TODO smokienko start parse tasks
+    }
+
+    public void setTaskQuestionFromJson(JSONObject objext){
+        //TODO smokienko end parse tasks
+    }
+
+
+
 }
--- NFCq/src/com/chernicka/model/User.java
+++ NFCq/src/com/chernicka/model/User.java
@@ -1,7 +1,79 @@
 package com.chernicka.model;
 
+import org.json.JSONObject;
+
 /**
  * Created by Sov on 26.10.13.
  */
 public class User {
+
+    private String id;
+    private String name;
+    private String pass;
+    private String key;
+    private String score;
+
+    private int latitude;
+    private int longtitude;
+
+    public User(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public int getLongtitude() {
+        return longtitude;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongtitude(int longtitude) {
+        this.longtitude = longtitude;
+    }
+
+    public void setUserDataFromJson(JSONObject object){}
+
 }
